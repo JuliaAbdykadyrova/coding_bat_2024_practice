0a7fada9ead50488f8a060c7c0d4ad479b7029f8
unknown <you@example.com>
2025-02-17 12:22:44 +0600

    Julia/warmup2

--- src/main/java/com/digital_nomads/julia/warmup1/Diff21.java
+++ src/main/java/com/digital_nomads/julia/warmup1/Diff21.java
@@ -1,4 +1,16 @@
 package com.digital_nomads.julia.warmup1;
 
 public class Diff21 {
+
+    public int diff21(int n) {
+        if (n >= 21) {
+            return (n - 21) * 2;
+        } else {
+            return 21 - n;
+        }
+
+    }
+
 }
+
+
